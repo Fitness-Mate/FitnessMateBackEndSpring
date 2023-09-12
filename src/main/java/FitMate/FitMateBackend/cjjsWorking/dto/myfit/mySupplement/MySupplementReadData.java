092e44b209f80c2f95ce9ab1cf1985e19fa67a8c
zzzzseong <jung48182@gmail.com>
2023-09-12 18:35:50 +0900

    feat(MySupplement): 루틴에 속한 보조제 리스트 조회 응답 필드 추가

--- src/main/java/FitMate/FitMateBackend/cjjsWorking/dto/myfit/mySupplement/MySupplementReadData.java
+++ src/main/java/FitMate/FitMateBackend/cjjsWorking/dto/myfit/mySupplement/MySupplementReadData.java
@@ -3,7 +3,7 @@ package FitMate.FitMateBackend.cjjsWorking.dto.myfit.mySupplement;
 import FitMate.FitMateBackend.cjjsWorking.service.storageService.S3FileService;
 import FitMate.FitMateBackend.consts.ServiceConst;
 import FitMate.FitMateBackend.domain.myfit.MySupplement;
-import FitMate.FitMateBackend.domain.supplement.Supplement;
+import FitMate.FitMateBackend.domain.supplement.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,11 +20,31 @@ public class MySupplementReadData {
     private String imageURL;
     private String description;
     private String supplementType;
-    private float servings;
-    //    private float proteinPerServing;
-//    private float carbohydratePerServing;
-    private String marketURL;
     private String flavor;
+    private int price;
+
+    // protein, Gainer
+    private Float proteinPerServing;
+    private Float fatPerServing;
+    private Float carbohydratePerServing;
+    private String source;
+
+    // AminoAcid
+    private Float leucine;
+    private Float isoLeucine;
+    private Float valine;
+    private Float L_Carnitine;
+    private Float L_Glutamine;
+    private Float L_Alanine;
+    private Float L_Lysine;
+    private Float methionine;
+    private Float phenylalanine;
+    private Float threonine;
+    private Float histidine;
+    private Float tryptophan;
+
+    //Other
+    private String contains;
 
     public MySupplementReadData(MySupplement mySupplement) {
         this.mySupplementId = mySupplement.getId();
@@ -36,9 +56,43 @@ public class MySupplementReadData {
         this.imageURL = S3FileService.getAccessURL(ServiceConst.S3_DIR_SUPPLEMENT, supplement.getImageName());
         this.description = supplement.getDescription();
         this.supplementType = supplement.getType().toString();
-        this.servings = supplement.getServings();
-        this.marketURL = supplement.getMarketURL();
         this.flavor = supplement.getFlavor();
+        this.price = supplement.getPrice();
+
+        if (supplement instanceof Gainer) {
+            this.supplementType = "Gainer";
+            Gainer sup = (Gainer) supplement;
+            this.proteinPerServing = sup.getProteinPerServing();
+            this.fatPerServing = sup.getFatPerServing();
+            this.carbohydratePerServing = sup.getCarbohydratePerServing();
+            this.source = sup.getSource();
+        } else if(supplement instanceof Protein) {
+            this.supplementType = "Protein";
+            Protein sup = (Protein) supplement;
+            this.proteinPerServing = sup.getProteinPerServing();
+            this.fatPerServing = sup.getFatPerServing();
+            this.carbohydratePerServing = sup.getCarbohydratePerServing();
+            this.source = sup.getSource();
+        } else if (supplement instanceof AminoAcid) {
+            this.supplementType = "AminoAcid";
+            AminoAcid sup = (AminoAcid) supplement;
+            this.leucine = sup.getLeucine();
+            this.isoLeucine = sup.getIsoLeucine();
+            this.valine = sup.getValine();
+            this.L_Carnitine = sup.getL_Carnitine();
+            this.L_Glutamine = sup.getL_Glutamine();
+            this.L_Alanine = sup.getL_Alanine();
+            this.L_Lysine = sup.getL_Lysine();
+            this.methionine = sup.getMethionine();
+            this.phenylalanine = sup.getPhenylalanine();
+            this.threonine = sup.getThreonine();
+            this.histidine = sup.getHistidine();
+            this.tryptophan = sup.getTryptophan();
+        } else if (supplement instanceof Other) {
+            this.supplementType = "Other";
+            Other sup = (Other) supplement;
+            this.contains = sup.getContains();
+        }
     }
 
 }
